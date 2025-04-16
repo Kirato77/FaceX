5bd1c72e32ff75759ab796738e822d879c961186
jer3m01 <jer3m01@jer3m01.com>
2025-04-16 13:55:31 +0200

    test: vitest browser mode

--- src/components/todo-list.test.tsx
+++ src/components/todo-list.test.tsx
@@ -1,5 +1,8 @@
 import { fireEvent, render } from "@solidjs/testing-library";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
+
+const user = userEvent.setup();
 
 import { TodoList } from "./todo-list";
 
@@ -20,26 +23,9 @@ describe("<TodoList />", () => {
 		const input = getByPlaceholderText("new todo here") as HTMLInputElement;
 		const button = getByText("Add Todo");
 		input.value = "test new todo";
-		fireEvent.click(button as HTMLInputElement);
+		await user.click(button as HTMLInputElement);
 		expect(input.value).toBe("");
 		expect(getByText(/test new todo/)).toBeInTheDocument();
-		unmount();
-	});
-
-	test("it will mark a todo as completed", async () => {
-		const { getByPlaceholderText, findByRole, getByText, unmount } = render(
-			() => <TodoList />,
-		);
-		const input = getByPlaceholderText("new todo here") as HTMLInputElement;
-		const button = getByText("Add Todo") as HTMLButtonElement;
-		input.value = "mark new todo as completed";
-		fireEvent.click(button);
-		const completed = (await findByRole("checkbox")) as HTMLInputElement;
-		expect(completed?.checked).toBe(false);
-		fireEvent.click(completed);
-		expect(completed?.checked).toBe(true);
-		const text = getByText("mark new todo as completed") as HTMLSpanElement;
-		expect(text).toHaveStyle({ "text-decoration": "line-through" });
 		unmount();
 	});
 });
